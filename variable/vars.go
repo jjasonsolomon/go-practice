f4f3ffc562ae98b7a57011fd15b4d1cc588d0407
Jason Solomon <jasonsolomon@Jasons-MacBook-Air.local>
2022-01-22 13:49:43 +0530

    Added Type conversion

--- variable/vars.go
+++ variable/vars.go
@@ -1,16 +1,34 @@
 package main
 
-import "fmt"
-
-var (
-
-	name, course string
-	module, clip int
+import (
+	"fmt"
+	"reflect"
+	"strconv"
 )
 
+var (
+	name   = "Jason"
+	course = "Go fundamentals"
+	module = "4"
+	clip   = 2
+)
 
-func main ()  {
-	
+func main() {
+
 	fmt.Println("Name and course are set to", name, "and", course)
-	fmt.Println("Module and clip are set to",module, "and", clip)
+	fmt.Println("Module and clip are set to", module, "and", clip)
+	//Giving Type Output
+	fmt.Println("Type of module", reflect.TypeOf(module))
+	fmt.Println("Type of name", reflect.TypeOf(name))
+	//total := module + clip
+	//fmt.Println("Total:-", total)
+
+	iModule, err := strconv.Atoi(module)
+	if err == nil {
+
+		total := iModule + clip
+		fmt.Println("Total:-", total)
+
+	}
+
 }
