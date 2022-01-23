1d429a65bfe9ca811f3221e536e16a36c39dbccd
Jason Solomon <jasonsolomon@Jasons-MacBook-Air.local>
2022-01-23 13:14:04 +0530

    Short Declation

--- variable/vars.go
+++ variable/vars.go
@@ -6,22 +6,18 @@ import (
 	"strconv"
 )
 
-var (
-	name   = "Jason"
-	course = "Go fundamentals"
-	module = "4"
-	clip   = 2
-)
-
 func main() {
+
+	name := "Jason"
+	course := "Go fundamentals"
+	module := "4"
+	clip := 2
 
 	fmt.Println("Name and course are set to", name, "and", course)
 	fmt.Println("Module and clip are set to", module, "and", clip)
 	//Giving Type Output
 	fmt.Println("Type of module", reflect.TypeOf(module))
 	fmt.Println("Type of name", reflect.TypeOf(name))
-	//total := module + clip
-	//fmt.Println("Total:-", total)
 
 	iModule, err := strconv.Atoi(module)
 	if err == nil {
