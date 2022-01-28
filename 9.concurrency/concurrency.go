67e3b799002662b505729a38a70659fe218807a8
Jason Solomon <jasonsolomon@Jasons-MacBook-Air.local>
2022-01-28 21:23:29 +0530

    Added concurrency with go routine

---
+++ 9.concurrency/concurrency.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	func() {
+		time.Sleep(5 * time.Second)
+		fmt.Println("Hello")
+	}()
+
+	func() {
+
+		fmt.Println("World")
+	}()
+}
