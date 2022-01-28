1ffcadda0f727b57f333cd52a4ec69c29505ad4b
Jason Solomon <jasonsolomon@Jasons-MacBook-Air.local>
2022-01-28 21:26:14 +0530

    concurrency updated with go-routine

--- 9.concurrency/concurrency.go
+++ 9.concurrency/concurrency.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 
-	func() {
+	var waitGrp sync.WaitGroup
+	waitGrp.Add(2)
+
+	go func() {
+
+		defer waitGrp.Done()
 		time.Sleep(5 * time.Second)
 		fmt.Println("Hello")
 	}()
 
-	func() {
-
+	go func() {
+		defer waitGrp.Done()
 		fmt.Println("World")
 	}()
+
+	waitGrp.Wait()
 }
